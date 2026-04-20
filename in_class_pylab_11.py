5c72b21282a413fe432fe3733993f672c3ee1a3d
DavTorCR <hollow.prolifc.vegabond@gmail.com>
2026-04-20 15:34:51 -0700

    the start.

---
+++ in_class_pylab_11.py
@@ -0,0 +1,107 @@
+print('hello world.')
+# focus on the second chaptes material on object oriented
+# programming.
+# "It seems like the book is trying to add a stepping stone, but I dont think it is neccesary.a
+# in other object oriented programming langueges you
+# have to add the attributes in the class definition.
+# "you add objects inside of objects."
+# " recursive objects "
+
+# "we are adding a mindmap, we will be composing
+# recursive objects."
+
+# "the whole point of object oriented programing
+# is to MUTATE the object."
+
+# be midnful to what objects are mutable and
+# what is IMMUTABLE
+# the copy module has a shallow copy and a deep copy,
+# the shallow copy will not have the nested objects.
+# a deep copy copies deeply, all the nested objects.
+# if mutbale object, it is going to be passed by reference.
+# immutable object, it is going to be completely copied.
+
+# pure function (will be tested) :def:
+
+# function and methods are a slight distinction;
+# to be explored next week.
+
+# there has been a recent push for functionable programming.
+
+# "-~~~the year 2000!~~~-no one go the reference?
+# (conan o brien)"
+
+# "anyone here the good news on robotics? "
+
+# "I try not to focus on trump..."
+
+# "the chinese had a robot run a half marathon
+# against people and it did 7 min faster "
+
+#ins Hartman brings up BBC news article, and shows
+# the video of it running
+
+# "you guys can do the practice at home."
+
+# (using the notes)
+
+# __inint__ is a special method that allows you
+# to CONSTRUCT object class.
+
+# the whole point of class objects is to obfuscate
+# the data that a user should not see.
+# or for encapsulation.
+
+
+# function and methods are similar, but;
+# method is inside of an object class definition.
+# ome terms to understand
+# 4
+
+# 4 inheritance
+
+
+
+# the three tyoes of python methods
+# static methods; they can exist outside of a
+# defined class, they are swiftly loaded into memory.
+
+# instance methods; inside a class using a slef definition
+
+# class methods; self modifying code,
+# python is the first to dynamically modify itself
+
+# *special methods; ??
+# they created by pythons language engineer,
+# overrides of the ordinary operators,
+
+
+print("This is the start of the Python Lab 11; ")
+
+"we are building a mind map today!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
